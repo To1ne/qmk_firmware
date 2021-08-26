03ac55354e18c350dce0d4cf90f44679d999f928
Toon Claes <toon@iotcl.com>
2021-08-26 20:29:46 +0200

    Add @to1ne's config for foostan/cornelius
    Initial version of my personal configuration used on the Cornelius
    keyboard.

---
+++ users/to1ne/rules.mk
@@ -0,0 +1,10 @@
+SRC += to1ne.c combos.c
+
+COMBO_ENABLE = yes
+NKRO_ENABLE = yes
+#EXTRAKEY_ENABLE = yes
+MOUSEKEY_ENABLE = yes
+
+ifeq ($(strip $(KEYBOARD)), foostan/cornelius)
+  SRC += keymap_to1ne_4x12.c
+endif
