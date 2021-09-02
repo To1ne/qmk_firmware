1b5737e6bccf8a2cb078e74456c4748f0c6f2e29
Toon Claes <toon@iotcl.com>
2021-09-02 15:55:25 +0200

    Use macro to build home mods directly into LAYOUT
    Instead of defining a macro for each key on each layer individually, add
    a LAYOUT derived macro that wraps the keycodes with the correct Mod-Tap
    macros. This will ensure the home row mods to be equal on each layer and
    less custom defined macros.

--- users/to1ne/to1ne.h
+++ users/to1ne/to1ne.h
@@ -1,30 +1,3 @@
 #pragma once
 
 #include QMK_KEYBOARD_H
-
-#define LG_A LGUI_T(KC_A)
-#define LA_R LALT_T(KC_R)
-#define LS_S LSFT_T(KC_S)
-#define LC_T LCTL_T(KC_T)
-
-#define RC_N RCTL_T(KC_N)
-#define RS_E RSFT_T(KC_E)
-#define RA_I RALT_T(KC_I)
-#define RG_O RGUI_T(KC_O)
-
-
-#define LG_XXXX LGUI_T(XXXXXXX)
-#define LA_XXXX LALT_T(XXXXXXX)
-#define LS_XXXX LSFT_T(XXXXXXX)
-#define LC_XXXX LCTL_T(XXXXXXX)
-
-#define RC_DOWN RCTL_T(KC_DOWN)
-#define RS_UP   RSFT_T(KC_UP)
-#define RA_RGHT RALT_T(KC_RGHT)
-#define RG_XXXX RGUI_T(XXXXXXX)
-
-
-#define RC_EQL  RCTL_T(KC_EQL)
-#define RS_LCBR RSFT_T(KC_LCBR)
-#define RA_RCBR RALT_T(KC_RCBR)
-#define RG_PIPE RGUI_T(KC_PIPE)
