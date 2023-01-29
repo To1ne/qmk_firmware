e2d50e39eb5776c6806e2efee97ad2be81285068
Toon Claes <toon@iotcl.com>
2023-01-29 20:23:26 +0100

    Make it better

--- users/to1ne/rules.mk
+++ users/to1ne/rules.mk
@@ -5,6 +5,6 @@ NKRO_ENABLE = yes
 #EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 
-ifeq ($(strip $(KEYBOARD)), foostan/cornelius)
-  SRC += keymap_to1ne_4x12.c
+ifeq ($(strip $(KEYBOARD)),foostan/cornelius)
+  #SRC += keymap_to1ne_4x12.c
 endif
