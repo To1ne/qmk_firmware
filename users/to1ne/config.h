fa6e745457b42bd6303b214b9ef004ff18a3ead2
Toon Claes <toon@iotcl.com>
2026-03-10 15:22:13 +0100

    users/to1ne: avoid accidental modifier combos

--- users/to1ne/config.h
+++ users/to1ne/config.h
@@ -1,3 +1,15 @@
 #pragma once
 
 #define TAPPING_TERM 125
+
+/*
+ * Enable Hold On Other Key Press to avoid accidental modifier combos.
+ * https://docs.qmk.fm/tap_hold#hold-on-other-key-press
+ */
+#define HOLD_ON_OTHER_KEY_PRESS
+
+/*
+ * Enable Permissive hold in an attempt to fix my C-g issue in Emacs
+ * https://docs.qmk.fm/#/tap_hold?id=permissive-hold
+ */
+//#define PERMISSIVE_HOLD
