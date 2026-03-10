d5f60128b26f65550266735c7fc9495aeea63bb7
Toon Claes <toon@iotcl.com>
2026-03-10 17:18:28 +0100

    users/to1ne: add note about dynamic tapping term
    See more at https://docs.qmk.fm/tap_hold#dynamic-tapping-term

--- users/to1ne/rules.mk
+++ users/to1ne/rules.mk
@@ -3,6 +3,10 @@ SRC += to1ne.c
 NKRO_ENABLE = yes
 #EXTRAKEY_ENABLE = yes
 MOUSEKEY_ENABLE = yes
+
+# Change the tapping term with DT_UP & DT_DOWN
+# To print it, press DT_PRNT
+# https://docs.qmk.fm/tap_hold#dynamic-tapping-term
 DYNAMIC_TAPPING_TERM_ENABLE = yes
 
 ifeq ($(strip $(KEYBOARD)),foostan/cornelius)
